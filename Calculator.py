78e6135ed8e2828de4a692aec93488c484926179
MeghaMane18 <meghamane575@gmail.com>
2026-06-30 22:34:12 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,32 @@
+while True:
+        print("Welcome to my Calculator!")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Exit")
+        choice = input("Enter your choice (1-5): ")
+        if choice == "5":
+                print("Thank you for using my calculator!")
+                break
+        else:
+         print("You selected option:", choice)  
+        num1 = float(input("Enter first number:"))
+        num2 = float(input("Enter second number:"))
+        if choice == "1":
+           result = num1 + num2
+           print("Result =", result)
+        elif choice == "2":
+            result = num1 - num2
+            print("Result =", result)
+        elif choice == "3":
+             result = num1 * num2
+             print("Result =", result)
+        elif choice == "4":
+            if num2 != 0:
+               result = num1 / num2
+               print("Result =", result)
+            else:
+               print("Error: Division by zero is not allowed.")
+        else:
+               print("Invalid choice! Please enter a number from 1 to 4.")
